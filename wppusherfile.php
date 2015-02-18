c1b5795b43960a822f608dc9614731e2f2d5ed85
Peter Suhm <peter@suhm.dk>
2015-02-18 12:50:14 +0100

    Update wppusherfile.php

--- wppusherfile.php
+++ wppusherfile.php
@@ -7,10 +7,10 @@ return array(
         'composer install',
     ),
 
-    // Defaults to false and is not required.
-    // If set to true, installing and update can take a long time
+    // Defaults to true and is not required.
+    // If set to false, installing and update can take a long time
     // and may seem like it's 'hanging'.
-    'async' => true,
+    'async' => false,
     
     // Defaults to true, meaning that if one command fails,
     // the following commands won't be executed.
