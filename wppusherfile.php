64b69bdaf79d98db16961a90ede89ac2af3516b8
Peter Suhm <peter@suhm.dk>
2015-02-18 13:32:40 +0100

    Update wppusherfile.php

--- wppusherfile.php
+++ wppusherfile.php
@@ -10,9 +10,9 @@ return array(
     // Defaults to true and is not required.
     // If set to false, installing and update can take a long time
     // and may seem like it's 'hanging'.
-    'async' => false,
+    // 'async' => false,
     
     // Defaults to true, meaning that if one command fails,
     // the following commands won't be executed.
-    'halt_on_fail' => false,
+    // 'halt_on_fail' => false,
 );
