bae07c11faf5510c72b9fe415af77ef4d81483e1
Peter Suhm <peter@suhm.dk>
2015-02-19 10:17:10 +0100

    Update wppusher.php

--- wppusher.php
+++ wppusher.php
@@ -4,7 +4,7 @@ return array(
 
     // Each string will be passed to the shell as a command.
     'commands' => array(
-        'composer install',
+        'touch tmp.file',
     ),
 
     // Defaults to true and is not required.
